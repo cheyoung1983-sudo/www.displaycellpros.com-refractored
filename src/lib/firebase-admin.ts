83cbab91e33584a61fcdb25709339d2ab68fe70c
cheyoung1983-sudo <cheyoung1983@gmail.com>
2026-07-05 17:24:53 -0700

    delo

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,11 @@
 import { initializeApp, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import firebaseConfig from "../../firebase-applet-config.json";
 
 if (!getApps().length) {
   initializeApp({
-    projectId: firebaseConfig.projectId,
-    databaseURL: (firebaseConfig as any).databaseURL || `https://${firebaseConfig.projectId}-default-rtdb.firebaseio.com`,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
   });
 }
 
