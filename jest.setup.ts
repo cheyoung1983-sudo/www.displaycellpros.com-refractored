9aa0c59104f437c25c63669c857571d5799f3d57
cheyoung1983-sudo <cheyoung1983@gmail.com>
2026-08-02 13:01:36 -0700

    chore: polyfill global Request for edge middleware tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,16 @@
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
+// Polyfills for Edge runtime globals used in middleware tests
+declare global {
+  var Request: any;
+  var Response: any;
+}
+// Minimal stub classes
+// @ts-ignore – attach to global
+global.Request = class {};
+// @ts-ignore – attach to global
+global.Response = class {};
+
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder as any;
